835be19efe947af9ad6cb02ddd096dabe1ec5fcb
SurajBhalerao27 <cs.engr.suraj@gmail.com>
2024-03-29 18:43:06 +0530

    initial commit on setting up files and connect to db

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+import connectDB from "./db/connection.js";
+
+dotenv.config({
+    path: "./env"
+});
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME}from "./constants";
+const app = express();
+( async()=>{
+try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    // TODO: see this again
+    app.on("error", err => console.log(err));  
+
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Server is running on port 3000");
+    });
+} catch (error) {
+    console.error("Error: " + error)
+    throw err
+}
+})()
+*/ 
+
+// This is effie ()() which have this two brackets for initialize and execute the same function on same time.
